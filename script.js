5c96d8bb10bc4e4c39055d9834b33eb5169d7ab2
mohamadreza dakhili <mohamadrezadakhili53@gmail.com>
2020-04-02 01:58:50 +0430

    finished quiz nine and three

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function convertDatePersian() {
 }
 
 // answer two question
-dt1 = new Date("2019/5/28 12:02:02");
-dt2 = new Date("2020/7/29 13:03:03");
-function diffTime(dt1, dt2) {
+let dt1 = new Date("2019/5/28 12:02:02");
+document.getElementById("oneDate").innerHTML ="One Date : " +dt1;
+let dt2 = new Date("2020/7/29 13:03:03");
+document.getElementById("twoDate").innerHTML = "Two Date : " + dt1;
+function diffTime() {
   let seconds = (dt2.getTime() - dt1.getTime()) / 1000;
   let min = Math.floor(seconds / 60);
   seconds = seconds % 60;
@@ -23,7 +25,7 @@ function diffTime(dt1, dt2) {
   let year = Math.floor(month / 12);
   month = month % 12;
   // print on tag p
-  document.getElementById("textDate").innerHTML =
+  document.getElementById("textDate").innerHTML = "Diff between two dates = " + 
     "years:" +
     year +
     " month:" +
@@ -37,25 +39,54 @@ function diffTime(dt1, dt2) {
     " seconds:" +
     seconds;
 }
-diffTime(dt1, dt2);
-// click button and change display tag p and show text
-function showText() {
-  var x = document.getElementById("textDate");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
 
 // answer three question
-array = [
+let array = [
   {
-    start: new Date("2019/5/28 12:02:02") , 
-    end : new Date("2020/7/29 13:03:03")
+    start: new Date("2020/7/29 13:03:00"),
+    end: new Date("2020/7/29 13:03:01")
+  },
+  {
+    start: new Date("2020/7/29 13:03:00"),
+    end: new Date("2020/7/29 13:03:02")
   }
 ];
-alert(array)
+document.getElementById("demo").innerHTML ="Array List = " + JSON.stringify(array, null, 4);
+
+function differenceTime() {
+  let second = 0;
+  for (let i = 0; i < array.length; i++) {
+    let start = array[i].start;
+    let end = array[i].end;
+    let time = (end.getTime() - start.getTime()) / 1000;
+    second += time;
+  }
+  let min = Math.floor(second / 60);
+  second = second % 60;
+  let hours = Math.floor(min / 60);
+  min = min % 60;
+  let day = Math.floor(hours / 24);
+  hours = hours % 24;
+  let month = Math.floor(day / 30);
+  day = day % 30;
+  let year = Math.floor(month / 12);
+  month = month % 12;
+  // print on tag p
+  document.getElementById("demo2").innerHTML =
+    "Time difference = " +
+    "years:" +
+    year +
+    " month:" +
+    month +
+    " days:" +
+    day +
+    " hours:" +
+    hours +
+    " minutes:" +
+    min +
+    " seconds:" +
+    second;
+}
 
 // answer four question
 let asiaTime = new Date().toLocaleString("en-US", {
@@ -169,10 +200,17 @@ function displayCars() {
 }
 
 // answer nine question
+function RangeNumber() {
+  let number = document.getElementById("numberRange").value;
+  let newNumber = number / 20;
+  document.getElementById("newNumber").innerHTML = "Number = " + newNumber;
+}
+
+// answer ten question
 let date = new Date();
-document.getElementById("dateTehran").innerHTML ="Date Tehran : "+ date;
+document.getElementById("dateTehran").innerHTML = "Date Tehran : " + date;
 
 function changeTime() {
   date.setMinutes(date.getMinutes() + 30);
-document.getElementById("newTime").innerHTML = "New Time : " + date;
+  document.getElementById("newTime").innerHTML = "New Time : " + date;
 }
